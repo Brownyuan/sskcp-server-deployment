17937c466addf7cc3250e695dc59fbcce28b8f25
AP1001 <Alejandro.Perez1001@protonmail.com>
2018-08-20 10:30:59 +0800

    Combine config path and config; omit port when login with sshkey

--- config_deploy.sh
+++ config_deploy.sh
@@ -1,48 +1,49 @@
 #!/bin/bash
 
-DIR="$1"
-CONFIG="$2"
-VPS1=$3
-VPS2=$4
-VPS3=$5
-VPS4=$6
+CONFIG="$1"
+machine1=$2
+machine2=$3
+machine3=$4
+machine4=$5
 
 
 
 usage(){
-	echo "$0 PARAMs"
+	echo "Usage: $0 PARAMs"
+	echo " "
+	echo "Put configuration directory to all machines"
 	echo " "
 	echo "Params:"
-	echo "  path" "  Path to config dir"
-	echo "  config" "Config dir"
-	echo "  vps1" "  Username@Password:port"
-	echo "  vps2" "  Username@Password:port"
-	echo "  vps3" "  Username@Password:port"
-	echo "  vps4" "  Username@Password:port"
+	echo "  config" "Configuration dir"
+	echo "  machine1" "  Username@Password:port or label"
+	echo "  machine2" "  Username@Password:port or label"
+	echo "  machine3" "  Username@Password:port or label"
+	echo "  machine4" "  Username@Password:port or label"
 	exit 1
 }
 
 
-if [ $# != 6 ];
+if [ $# != 5 ];
 then
 	usage
 	exit 1
 fi
 
-vps_list=("${VPS1}" "${VPS2}" "${VPS3}" "${VPS4}")
+machine_list=("${machine1}" "${machine2}" "${machine3}" "${machine4}")
 
-for vps in ${vps_list[@]}
+for machine in ${machine_list[@]}
 do
-	#echo ${vps}
-	PORT=${vps#*:}
-	#echo ${vps}
-	ser=${vps%:*}
+	flag=`echo $machine|awk '{print match($0,":")}'`;
+	echo $flag
+	if [ $flag -gt 0 ];then
+		PORT=${machine#*:}
+		ser=${machine%:*}
+		scp -P ${PORT} -r ${CONFIG} ${ser}:~
+	    ssh -p ${PORT} -t ${ser} "sudo mkdir /app_data ;sudo mkdir /app_data/sskcp_conf; sudo cp -r ~/sskcp_conf /app_data; sudo tree /app_data ;"
 
-	#echo $vps
-	#echo $PORT
-	scp -P ${PORT} -r ${DIR}/${CONFIG} ${ser}:~
-	#echo ${ser}
-	#echo $PORT
-	ssh -p ${PORT} -t ${ser} "sudo mkdir /app_data ;sudo mkdir /app_data/sskcp_conf; sudo cp -r ~/sskcp_conf /app_data; sudo tree /app_data ; sudo tree ~;"
+	else
+		scp -r ${CONFIG} ${machine}:~
+	    ssh -t ${machine} "sudo mkdir /app_data ;sudo mkdir /app_data/sskcp_conf; sudo cp -r ~/sskcp_conf /app_data; sudo tree /app_data ;"
+	fi
 done
 
--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 
-DIR="$1"
-COMPOSEFILE="$2"
-MANAGERFILE="$3"
-MANAGER="$4"
-PORT=${MANAGER#*:}
-ser=${MANAGER%:*}
+COMPOSEFILE="$1"
+MANAGERFILE="$2"
+MANAGER="$3"
 
 usage(){
 	echo "Usage: $0 PARAMs"
 	echo " "
+	echo "Deploy docker serice to swarm"
+	echo " "
 	echo "Params:"
-	echo "  path" "        Path to docker compose file"
 	echo "  composefile" " Dokcer compose file"
 	echo "  managerfile" " json file for manager ssh"
 	echo "  manager" "     manager ssh info"
@@ -20,7 +18,7 @@ usage(){
 }
 
 
-if [ $# != 4 ];
+if [ $# != 3 ];
 then
 	usage
 	exit 1
@@ -30,6 +28,19 @@ fi
 
 echo "scp docker-compose file to manager"
 scp -P ${PORT} -r ${DIR}/${COMPOSEFILE} ${ser}:~
+
+
+flag=`echo $MANAGER|awk '{print match($0,":")}'`;
+
+if [ $flag -gt 0 ];then
+	PORT=${MANAGER#*:}
+	ser=${MANAGER%:*}
+	scp -P ${PORT} -r ${COMPOSEFILE} ${ser}:~
+else
+	scp -r ${COMPOSEFILE} ${MANAGER}:~
+fi
+
+
 
 echo "Deploy ss-kcp"
 ./ssh-rpc-agent --mf ${MANAGERFILE} --tf tasks/deploy.json
